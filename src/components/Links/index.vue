383631a83c3aa436359737bf02356a0aa1812352
lisichaoyun <lisichaoyun@163.com>
2023-02-23 15:10:03 +0800

    第二次

---
+++ src/components/Links/index.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="links">
+    <div class="line">
+      <Icon size="20">
+        <Link />
+      </Icon>
+      <span class="title">功能列表</span>
+    </div>
+    <el-row class="link-all" :gutter="20">
+      <el-col
+        :span="8"
+        v-for="(item, index) in linksData"
+        :key="item"
+        @click="jumpLink(item.link)"
+      >
+        <div
+          class="item cards"
+          :style="index < 3 ? 'margin-bottom: 20px' : null"
+        >
+          <Icon size="26">
+            <component :is="item.icon" />
+          </Icon>
+          <span class="name">{{ item.name }}</span>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { Icon } from "@vicons/utils";
+import {
+  Link,
+  Blog,
+  CompactDisc,
+  Cloud,
+  Compass,
+  Book,
+  Flask,
+} from "@vicons/fa";
+
+// 网站链接数据
+// 建议不要超出6个，若需要超出请自行调整样式
+let linksData = [
+  {
+    icon: Blog,
+    name: "前端技术分享",
+    link: "https://www.cnblogs.com/SichaoyunLee/",
+  },
+  {
+    icon: Cloud,
+    name: "经验分享",
+    link: "https://www.cnblogs.com/SichaoyunLee/",
+  },
+  {
+    icon: CompactDisc,
+    name: "每日小日记",
+    link: "http://42.194.250.149:3600/",
+  },
+  {
+    icon: Compass,
+    name: "收藏网址",
+    link: "http://42.194.250.149:81/",
+  },
+  {
+    icon: Book,
+    name: "网站导航",
+    link: "http://42.194.250.149:82/",
+  },
+  {
+    icon: Flask,
+    name: "在线实验室",
+    link: "https://lab.imsyy.top/",
+  },
+];
+
+// 链接跳转
+const jumpLink = (url) => {
+  window.open(url, "_blank");
+};
+</script>
+
+<style lang="scss" scoped>
+.links {
+  .line {
+    margin: 2rem 0.25rem 1rem;
+    font-size: 1.1rem;
+    display: flex;
+    align-items: center;
+    animation: fade;
+    -webkit-animation: fade 0.5s;
+    .title {
+      margin-left: 8px;
+      font-size: 1.15rem;
+      text-shadow: 0 0 5px #00000050;
+    }
+  }
+  .link-all {
+    .item {
+      height: 100px;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+      justify-content: center;
+      animation: fade;
+      -webkit-animation: fade 0.5s;
+
+      &:hover {
+        transform: scale(1.02);
+        background: rgb(0 0 0 / 40%);
+        transition: 0.3s;
+      }
+      .name {
+        font-size: 1.1rem;
+        margin-left: 8px;
+      }
+      @media (min-width: 720px) and (max-width: 820px) {
+        .name {
+          display: none;
+        }
+      }
+      @media (max-width: 720px) {
+        height: 80px;
+      }
+      @media (max-width: 460px) {
+        flex-direction: column;
+        .name {
+          font-size: 1rem;
+          margin-left: 0;
+          margin-top: 8px;
+        }
+      }
+    }
+  }
+}
+</style>
